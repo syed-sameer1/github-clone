92e7e7427bff9935888731de0ed92d2ee57e70bd
syed-sameer1 <sameer.shah12570@gmail.com>
2019-11-24 20:11:21 +0500

    dont know 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./App.css";
 import Header from "./component/Header";
@@ -77,30 +76,6 @@ function App() {
     //     unreadNumber="2"
     //   />
     // </div>
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> parent of bc7f079... done
   );
 }
 
